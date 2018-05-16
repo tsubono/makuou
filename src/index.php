cdf6b9208db4ab483ac12f6e8d316801c8a4471b
Satoshi Shoji <shoji@ga-design.jp>
2018-05-16 11:55:30 +0900

    コーディングデータの追加（対象ページ：トップ、横断幕を作る、検索結果）

---
+++ src/index.php
@@ -0,0 +1,156 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>幕王</title>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="format-detection" content="telephone=no">
+    <link rel="shortcut icon" href="assets/img/favicon.ico">
+    <link rel="apple-touch-icon" href="assets/img/apple-touch-icon.png">
+    <link rel="stylesheet" type="text/css" href="assets/css/common.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/top.css">
+</head>
+<body>
+
+<?php include_once "inc/header.inc"; ?>
+
+<div class="bg">
+    <div class="mainimg sp_none">
+        <div class="img"><img src="assets/img/top/mainimg.png" alt="誰でも簡単にオリジナル横断幕デザインが作れます！幕デザインの幕王～幕王は試合を盛り上げるお手伝いをします～"></div>
+        <div class="btn"><a href="#"><img src="assets/img/top/main_btn.png" alt="オリジナル横断幕を作る"></a></div>
+    </div>
+    <div class="mainimg pc_none tab_none">
+        <div class="img"><a href="#"><img src="assets/img/top/mainimg-sp.png" alt="誰でも簡単にオリジナル横断幕デザインが作れます！幕デザインの幕王～幕王は試合を盛り上げるお手伝いをします～オリジナル横断幕を作る"></a></div>
+    </div>
+    <div class="pickup">
+        <ul class="cf">
+            <li>
+                <div><img src="assets/img/top/p_ico01.png" alt=""></div>
+                <p>最短2営業日で発送</p>
+            </li>
+            <li>
+                <div><img src="assets/img/top/p_ico02.png" alt=""></div>
+                <p>安心の高品質</p>
+            </li>
+            <li>
+                <div><img src="assets/img/top/p_ico03.png" alt=""></div>
+                <p>送料全国一律</p>
+            </li>
+            <li>
+                <div><img src="assets/img/top/p_ico04.png" alt=""></div>
+                <p>選べる支払方法</p>
+            </li>
+        </ul>
+    </div>
+    <div class="contents">
+        <section class="box3">
+            <h2>シーンから選ぶ</h2>
+            <ul class="cf">
+                <li><a href="#"><img src="assets/img/top/scene_01.png" alt="スポーツ応援"></a></li>
+                <li><a href="#"><img src="assets/img/top/scene_02.png" alt="お祝い・式典"></a></li>
+                <li><a href="#"><img src="assets/img/top/scene_03.png" alt="学校行事"></a></li>
+                <li><a href="#"><img src="assets/img/top/scene_04.png" alt="イベント・フェス"></a></li>
+                <li><a href="#"><img src="assets/img/top/scene_05.png" alt="ホームパーティー"></a></li>
+                <li><a href="#"><img src="assets/img/top/scene_06.png" alt="商売繁盛"></a></li>
+            </ul>
+        </section>
+        <section class="box5">
+            <h2>スポーツから選ぶ</h2>
+            <ul class="cf">
+                <li><a href="#"><img src="assets/img/top/sport_01.png" alt="サッカー・フットサル"></a></li>
+                <li><a href="#"><img src="assets/img/top/sport_02.png" alt="野球・ソフトボール"></a></li>
+                <li><a href="#"><img src="assets/img/top/sport_03.png" alt="バスケットボール"></a></li>
+                <li><a href="#"><img src="assets/img/top/sport_04.png" alt="バレーボール"></a></li>
+                <li><a href="#"><img src="assets/img/top/sport_05.png" alt="テニス"></a></li>
+                <li><a href="#"><img src="assets/img/top/sport_06.png" alt="バドミントン"></a></li>
+                <li><a href="#"><img src="assets/img/top/sport_07.png" alt="ラグビー"></a></li>
+                <li><a href="#"><img src="assets/img/top/sport_08.png" alt="ハンドボール"></a></li>
+                <li><a href="#"><img src="assets/img/top/sport_09.png" alt="ドッジボール"></a></li>
+                <li><a href="#"><img src="assets/img/top/sport_10.png" alt="卓球"></a></li>
+                <li><a href="#"><img src="assets/img/top/sport_11.png" alt="剣道"></a></li>
+                <li><a href="#"><img src="assets/img/top/sport_12.png" alt="弓道"></a></li>
+                <li><a href="#"><img src="assets/img/top/sport_13.png" alt="空手道・柔道"></a></li>
+                <li><a href="#"><img src="assets/img/top/sport_14.png" alt="陸上競技"></a></li>
+                <li><a href="#"><img src="assets/img/top/sport_15.png" alt="水泳"></a></li>
+                <li><a href="#"><img src="assets/img/top/sport_16.png" alt="体操"></a></li>
+                <li><a href="#"><img src="assets/img/top/sport_17.png" alt="スキー・スノーボード"></a></li>
+                <li><a href="#"><img src="assets/img/top/sport_18.png" alt="スケート"></a></li>
+                <li><a href="#"><img src="assets/img/top/sport_19.png" alt="レスリング"></a></li>
+                <li><a href="#"><img src="assets/img/top/sport_20.png" alt="ダンス"></a></li>
+            </ul>
+        </section>
+        <section class="box3">
+            <h2>テイストから選ぶ</h2>
+            <ul class="cf">
+                <li><a href="#"><img src="assets/img/top/taste_01.png" alt="シンプル素材少なめの定番柄"></a></li>
+                <li><a href="#"><img src="assets/img/top/taste_02.png" alt="熱血応援幕らしい炎モチーフ"></a></li>
+                <li><a href="#"><img src="assets/img/top/taste_03.png" alt="スポーティースポーツ幕らしいデザイン"></a></li>
+                <li><a href="#"><img src="assets/img/top/taste_04.png" alt="ナチュラル自然モチーフ"></a></li>
+                <li><a href="#"><img src="assets/img/top/taste_05.png" alt="インパクトとにかく目立ちたい人向け"></a></li>
+                <li><a href="#"><img src="assets/img/top/taste_06.png" alt="かわいいイラスト多めでポップなイメージ"></a></li>
+                <li><a href="#"><img src="assets/img/top/taste_07.png" alt="ヴィンテージ古風柄で大人向け"></a></li>
+                <li><a href="#"><img src="assets/img/top/taste_08.png" alt="ゴージャスキラキラや幻想的なデザイン"></a></li>
+                <li><a href="#"><img src="assets/img/top/taste_09.png" alt="和風日本柄"></a></li>
+            </ul>
+        </section>
+        <section class="feature">
+            <h2>選ばれる6つの特徴</h2>
+            <ul class="cf">
+                <li>
+                    <dl>
+                        <dt>誰でも<span class="blue">簡単に</span>オリジナル<br class="pc_none sp_none">デザインが作れる！</dt>
+                        <dd>デザインテンプレートよりお好みの編集が簡単にできます。<br class="sp_none">用途に合わせたデザイン制作が可能です！<br>自分で編集するので校正の時間を短縮でき、デザイン料無料です！</dd>
+                    </dl>
+                </li>
+                <li>
+                    <dl>
+                        <dt>最短<span class="blue">2営業日</span>後発送！</dt>
+                        <dd>15時までのご注文で2営業日後発送です。<br>特急(料金2割増し)なら翌営業日後発送で！どこにも負けない短納期で、週末のイベントに間に合います！</dd>
+                    </dl>
+                </li>
+                <li>
+                    <dl>
+                        <dt>自社専門工場での制作で<br class="pc_none"><span class="blue">安心の高品質</span>！</dt>
+                        <dd>印刷から縫製、発送まですべて自社工場で！豊富な取引実績から耐候性・発色にこだわった印刷方法や丈夫な縫製技術を研究開発し、お客様にご満足頂ける商品とサービス向上に努めています！</dd>
+                    </dl>
+                </li>
+                <li>
+                    <dl>
+                        <dt><span class="blue">見積り無料</span>で料金が<br class="pc_none sp_none">分かりやすい！</dt>
+                        <dd>幕のサイズと生地をご選択いただくと価格が決まります。<br>加工の変更は無料で行います。デザインは何種類でも制作できるので、ご満足いただいてから発注できます！</dd>
+                    </dl>
+                </li>
+                <li>
+                    <dl>
+                        <dt><span class="blue">送料全国一律</span>800円！</dt>
+                        <dd>1回のご注文で、全国どこでも送料一律です！<br>※発送先が複数ある場合、それぞれの納品先に送料がかかります。</dd>
+                    </dl>
+                </li>
+                <li>
+                    <dl>
+                        <dt><span class="blue">選べる支払方法</span>！<br class="pc_none sp_none">後払いOK！</dt>
+                        <dd>初めての方やお急ぎの方でも、お支払方法が選べます！<br>カード払い・コンビニ振込み・郵便局振込み・銀行振込みで、後払いが可能です。</dd>
+                    </dl>
+                </li>
+            </ul>
+        </section>
+        <div class="btn"><a href="#"><img src="assets/img/top/make_btn.png" alt="ネットでレイアウトを調整しながら自分でデザイン！オリジナル横断幕を作る"></a></div>
+    </div>
+</div>
+
+<?php include_once "inc/footer.inc"; ?>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="assets/js/common.js"></script>
+<script src="assets/js/jquery.matchHeight.js"></script>
+<script>
+    $(function(){
+        $(".pickup li").matchHeight();
+        $(".feature li").matchHeight();
+    });
+</script>
+
+</body>
+</html>
---
+++ src/search/index.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>横断幕を作る</title>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="format-detection" content="telephone=no">
+    <link rel="shortcut icon" href="../assets/img/favicon.ico">
+    <link rel="apple-touch-icon" href="../assets/img/apple-touch-icon.png">
+    <link rel="stylesheet" href="../assets/css/common.css">
+    <link rel="stylesheet" href="../assets/css/search.css">
+</head>
+<body>
+
+<?php include_once "../inc/header.inc"; ?>
+
+<main class="l-main">
+    <div class="l-inner">
+        <h1 class="main__title">
+            <picture>
+                <img src="../assets/img/search/title.png" srcset="../assets/img/search/title@2x.png 2x" alt="横断幕を作る">
+            </picture>
+        </h1>
+        <div class="main__content">
+            <ul class="main__breadcrumb">
+                <li><a href="">HOME</a></li>
+                <li>横断幕を作る</li>
+            </ul>
+            <section class="search">
+                <div class="search__inner">
+                    <h2 class="search__heading">
+                        <picture>
+                            <img src="../assets/img/search/heading--search.png" srcset="../assets/img/search/heading--search@2x.png 2x" alt="テンプレートを探す">
+                        </picture>
+                    </h2>
+                    <div class="search__form">
+                        <form method="" action="">
+                            <h3 class="search__item">縦横比率を選択する</h3>
+                            <ul class="search__option--radio">
+                                <li><input type="radio" name="aspect" id="aspect01" checked><label for="aspect01">1:1</label></li>
+                                <li><input type="radio" name="aspect" id="aspect02"><label for="aspect02">1:1.5</label></li>
+                                <li><input type="radio" name="aspect" id="aspect03"><label for="aspect03">1:2</label></li>
+                                <li><input type="radio" name="aspect" id="aspect04"><label for="aspect04">1:3</label></li>
+                                <li><input type="radio" name="aspect" id="aspect05"><label for="aspect05">1:4</label></li>
+                            </ul>
+                            <h3 class="search__item">スポーツを選ぶ</h3>
+                            <ul class="search__option--checkbox">
+                                <li><input type="checkbox" name="sports" id="sports01"><label for="sports01">1.サッカー・フットサル</label></li>
+                                <li><input type="checkbox" name="sports" id="sports02"><label for="sports02">2.野球・ソフトボール</label></li>
+                                <li><input type="checkbox" name="sports" id="sports03"><label for="sports03">3.バスケットボール</label></li>
+                                <li><input type="checkbox" name="sports" id="sports04"><label for="sports04">4.バレーボール</label></li>
+                                <li><input type="checkbox" name="sports" id="sports05"><label for="sports05">5.テニス</label></li>
+                                <li><input type="checkbox" name="sports" id="sports06"><label for="sports06">6.バトミントン</label></li>
+                                <li><input type="checkbox" name="sports" id="sports07"><label for="sports07">7.ラグビー</label></li>
+                                <li><input type="checkbox" name="sports" id="sports08"><label for="sports08">8.ハンドボール</label></li>
+                                <li><input type="checkbox" name="sports" id="sports09"><label for="sports09">9.ドッジボール</label></li>
+                                <li><input type="checkbox" name="sports" id="sports10"><label for="sports10">10.卓球</label></li>
+                                <li><input type="checkbox" name="sports" id="sports11"><label for="sports11">11.剣道</label></li>
+                                <li><input type="checkbox" name="sports" id="sports12"><label for="sports12">12.弓道</label></li>
+                                <li><input type="checkbox" name="sports" id="sports13"><label for="sports13">13.空手道・柔道</label></li>
+                                <li><input type="checkbox" name="sports" id="sports14"><label for="sports14">14.陸上競技</label></li>
+                                <li><input type="checkbox" name="sports" id="sports15"><label for="sports15">15.水泳</label></li>
+                                <li><input type="checkbox" name="sports" id="sports16"><label for="sports16">16.体操</label></li>
+                                <li><input type="checkbox" name="sports" id="sports17"><label for="sports17">17.スキー・スノーボード</label></li>
+                                <li><input type="checkbox" name="sports" id="sports18"><label for="sports18">18.スケート</label></li>
+                                <li><input type="checkbox" name="sports" id="sports19"><label for="sports19">19.レスリング</label></li>
+                                <li><input type="checkbox" name="sports" id="sports20"><label for="sports20">20.ダンス</label></li>
+                            </ul>
+                            <h3 class="search__item">テイストを選ぶ</h3>
+                            <ul class="search__option--checkbox">
+                                <li><input type="checkbox" name="taste" id="taste01"><label for="taste01">1.シンプル</label></li>
+                                <li><input type="checkbox" name="taste" id="taste02"><label for="taste02">2.熱血</label></li>
+                                <li><input type="checkbox" name="taste" id="taste03"><label for="taste03">3.スポーティ</label></li>
+                                <li><input type="checkbox" name="taste" id="taste04"><label for="taste04">4.ナチュラル</label></li>
+                                <li><input type="checkbox" name="taste" id="taste05"><label for="taste05">5.インパクト</label></li>
+                                <li><input type="checkbox" name="taste" id="taste06"><label for="taste06">6.かわいい</label></li>
+                                <li><input type="checkbox" name="taste" id="taste07"><label for="taste07">7.ヴィンテージ</label></li>
+                                <li><input type="checkbox" name="taste" id="taste08"><label for="taste08">8.ゴージャス</label></li>
+                                <li><input type="checkbox" name="taste" id="taste09"><label for="taste09">9.和風</label></li>
+                            </ul>
+                            <h3 class="search__item">シーンを選ぶ</h3>
+                            <ul class="search__option--checkbox">
+                                <li><input type="checkbox" name="scene" id="scene01"><label for="scene01">1.スポーツ応援</label></li>
+                                <li><input type="checkbox" name="scene" id="scene02"><label for="scene02">2.お祝い・式典</label></li>
+                                <li><input type="checkbox" name="scene" id="scene03"><label for="scene03">3.学校行事</label></li>
+                                <li><input type="checkbox" name="scene" id="scene04"><label for="scene04">4.イベント・フェス</label></li>
+                                <li><input type="checkbox" name="scene" id="scene05"><label for="scene05">5.ホームパーティ</label></li>
+                                <li><input type="checkbox" name="scene" id="scene06"><label for="scene06">6.商売繁盛</label></li>
+                            </ul>
+                            <input type="submit" value="検索">
+                        </form>
+                    </div>
+                </div>
+            </section>
+            <!-- /.search -->
+            <section class="pickup">
+                <h2 class="pickup__heading">
+                    <picture>
+                        <img src="../assets/img/search/heading--pickup.png" srcset="../assets/img/search/heading--pickup@2x.png 2x" alt="Pick Up!">
+                    </picture>
+                </h2>
+                <div class="pickup__content">
+                    <div class="pickup__box">
+                        <div>
+                            <img src="../assets/img/banner/banner01.png" alt="">
+                            <dl class="pickup__info">
+                                <dt>比率</dt>
+                                <dd>1:1.5</dd>
+                                <dt>サイズ</dt>
+                                <dd>600</dd>
+                            </dl>
+                        </div>
+                    </div>
+                    <div class="pickup__box">
+                        <div>
+                            <img src="../assets/img/banner/banner01.png" alt="">
+                            <dl class="pickup__info">
+                                <dt>比率</dt>
+                                <dd>1:1.5</dd>
+                                <dt>サイズ</dt>
+                                <dd>600</dd>
+                            </dl>
+                        </div>
+                    </div>
+                    <div class="pickup__box">
+                        <div>
+                            <img src="../assets/img/banner/banner01.png" alt="">
+                            <dl class="pickup__info">
+                                <dt>比率</dt>
+                                <dd>1:1.5</dd>
+                                <dt>サイズ</dt>
+                                <dd>600</dd>
+                            </dl>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- /.pickup -->
+        </div>
+    </div>
+</main>
+<!-- /.l-main -->
+
+<?php include_once "../inc/footer.inc"; ?>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="../assets/js/common.js"></script>
+
+</body>
+</html>

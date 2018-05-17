521d82e1a7419dd1646b46184f8ae703a3fd1ddb
Arabbit <tsupo222@yahoo.co.jp>
2018-05-17 11:21:31 +0900

    フロントレイアウト切り分け、特定商取引法表示ページ適応

--- routes/web.php
+++ routes/web.php
@@ -52,6 +52,7 @@ Route::namespace('Front')->group(function() {
     Route::get('/faq',      'FaqController@index')->name('faq');
     Route::get('/company',      'CompanyController@index')->name('company');
     Route::get('/contract',      'ContractController@index')->name('contract');
+    Route::get('/order',      'OrderController@index')->name('order');
 });
 /*
 |--------------------------------------------------------------------------

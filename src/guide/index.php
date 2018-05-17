90912146492cc568394383388e47b4b4b1ad5228
Satoshi Shoji <shoji@ga-design.jp>
2018-05-17 10:30:51 +0900

    コーディングデータの追加（対象ページ：ご利用ガイド）

---
+++ src/guide/index.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>ご利用ガイド</title>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="format-detection" content="telephone=no">
+    <link rel="shortcut icon" href="assets/img/favicon.ico">
+    <link rel="apple-touch-icon" href="assets/img/apple-touch-icon.png">
+    <link rel="stylesheet" href="../assets/css/common.css">
+    <link rel="stylesheet" href="../assets/css/search.css">
+	<link rel="stylesheet" href="../assets/css/layer.css">
+</head>
+<body>
+
+<?php include_once "../inc/header.inc"; ?>
+
+<main class="l-main">
+    <div class="l-inner guide">
+        <h1 class="main__title"><img src="../assets/img/guide/title.png" alt="ご利用ガイド"></h1>
+        <div class="main__content">
+            <ul class="main__breadcrumb">
+                <li><a href="">HOME</a></li>
+                <li>ご利用ガイド</li>
+            </ul>
+			
+            <section>
+                <h4 class="ttl01">ご利用ガイド</h4>
+				<div class="guide_flow">
+					<div class="fl_box">
+					<div class="fl_title">
+						<span class="fl_sh">STEP</span><span class="fl_bg">1</span>
+					</div>
+					<div class="fl_contents">
+						<div class="fl_img">
+						<div class="fl_icon1"></div>
+						</div>
+						<div class="fl_text">
+						<p class="fl_p">新規会員登録（無料）<br>して、ログイン</p>
+						</div>	
+					</div>				
+					</div>
+					
+					<div class="fl_box">
+					<div class="fl_title">
+						<span class="fl_sh">STEP</span><span class="fl_bg">2</span>
+					</div>
+					<div class="fl_contents">
+						<div class="fl_img">
+						<div class="fl_icon2"></div>
+						</div>
+						<div class="fl_text">
+						<p class="fl_p">デザイン<br>テンプレートを選ぶ</p>
+						</div>	
+					</div>				
+					</div>
+				
+					<div class="fl_box">
+					<div class="fl_title">
+						<span class="fl_sh">STEP</span><span class="fl_bg">3</span>
+					</div>
+					<div class="fl_contents">
+						<div class="fl_img">
+						<div class="fl_icon3"></div>
+						</div>
+						<div class="fl_text">
+						<p class="fl_p">デザインを作る</p>
+						</div>	
+					</div>				
+					</div>
+					
+					<div class="fl_box">
+					<div class="fl_title">
+						<span class="fl_sh">STEP</span><span class="fl_bg">4</span>
+					</div>
+					<div class="fl_contents">
+						<div class="fl_img">
+						<div class="fl_icon4"></div>
+						</div>
+						<div class="fl_text">
+						<p class="fl_p">ご注文手続き</p>
+						</div>	
+					</div>				
+					</div>
+				</div>				
+				
+                <h5 class="ttl02">納期・お届けについて</h5>
+                <h5 class="ttl03">通常のご注文</h5>
+                <p class="txt">
+                    15時までのご発注で、2営業日後で発送いたします。（※発注日は含みません）<br>
+                    特急発送では、料金2割増しで、翌営業日後で発送いたします。
+                </p>
+
+                <h5 class="ttl03">オーダーメイド</h5>
+                <p class="txt">
+                    デザインのご確定後、3営業日後で発送いたします。（※発注日は含みません）
+                </p>
+                <h5 class="ttl02">お支払方法</h5>
+                <p class="txt">
+                    クレジットカード：ご注文時にお支払い<br>
+                    コンビニ決済：後払い<br>
+                    郵便局・銀行振込：後払い→商品が届いてからお支払ください。請求書を一緒にお送りいたします。
+                </p>
+                <h5 class="ttl02">発送・送料について</h5>
+                <h5 class="ttl03">発送について</h5>
+                <p class="txt">
+                    発送後、お届けにかかる日数の目安は、本州四国（離島除く）は翌日、その他の地域は翌々日となります。<br>
+                    ※お届けをお約束するものではありません。ご了承ください。<br>
+                    ご使用の日程が決まっている場合は、ご注文時にお知らせ下さい。<br>
+                    商品の追跡をご希望の方は、お問合せください。
+                </p>
+                <h5 class="ttl03">送料について</h5>
+                <p class="txt">
+                    全国送料一律800円となります。<br>
+                    納品先が複数に分かれる場合は、納品先ごとに送料がかかります。
+                </p>
+            </section>    
+            <!-- /.search -->
+            <section class="pickup">
+                <h2 class="pickup__heading"><img src="../assets/img/search/heading--pickup.png" alt=""></h2>
+                <div class="pickup__content">
+                    <div class="pickup__box">
+                        <div>
+                            <img src="../assets/img/banner/banner01.png" alt="">
+                            <dl class="pickup__info">
+                                <dt>比率</dt>
+                                <dd>1:1.5</dd>
+                                <dt>サイズ</dt>
+                                <dd>600</dd>
+                            </dl>
+                        </div>
+                    </div>
+                    <div class="pickup__box">
+                        <div>
+                            <img src="../assets/img/banner/banner01.png" alt="">
+                            <dl class="pickup__info">
+                                <dt>比率</dt>
+                                <dd>1:1.5</dd>
+                                <dt>サイズ</dt>
+                                <dd>600</dd>
+                            </dl>
+                        </div>
+                    </div>
+                    <div class="pickup__box">
+                        <div>
+                            <img src="../assets/img/banner/banner01.png" alt="">
+                            <dl class="pickup__info">
+                                <dt>比率</dt>
+                                <dd>1:1.5</dd>
+                                <dt>サイズ</dt>
+                                <dd>600</dd>
+                            </dl>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- /.guide -->
+        </div>
+    </div>
+</main>
+<!-- /.l-main -->
+
+<?php include_once "../inc/footer.inc"; ?>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="../assets/js/common.js?v=1"></script>
+<script src="../assets/js/search.js"></script>
+</body>
+</html>

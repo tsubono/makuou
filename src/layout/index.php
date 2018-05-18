df54f644cad555f846bcd58a19d5785694593f46
Satoshi Shoji <shoji@ga-design.jp>
2018-05-18 11:07:37 +0900

    「レイアウトを作る」ページ追加、「よくあるご質問」ページ修正

---
+++ src/layout/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>レイアウトを作る</title>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="format-detection" content="telephone=no">
+    <link rel="shortcut icon" href="../assets/img/favicon.ico">
+    <link rel="apple-touch-icon" href="../assets/img/apple-touch-icon.png">
+    <link rel="stylesheet" href="../assets/css/common.css">
+    <link rel="stylesheet" href="../assets/css/search.css">
+    <link rel="stylesheet" href="../assets/css/layer.css">
+    
+</head>
+<body>
+
+<?php include_once "../inc/header.inc"; ?>
+
+<main class="l-main">
+    <div class="l-inner">
+        <section class="layout">
+            <h1 class="main__title"><img src="../assets/img/layout/title.png" alt="レイアウトを作る"></h1>
+            <div class="main__content">
+                <ul class="main__breadcrumb">
+                    <li><a href="">HOME</a></li>
+                    <li>レイアウトを作る</li>
+                </ul>
+                <div>
+                    <img src="../assets/img/layout/scrn.png" alt="デザイン編集画面">
+                </div>
+                <div class="btn">
+                    <a href="#">デザイン確認</a>
+                </div>
+            </div>
+        </section>    
+        <!-- /.search -->
+        <!-- /.pickup -->
+    </div>
+</main>
+<!-- /.l-main -->
+
+<?php include_once "../inc/footer.inc"; ?>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="../assets/js/common.js?v=1"></script>
+<script src="../assets/js/search.js"></script>
+</body>
+</html>

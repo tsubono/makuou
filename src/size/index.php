b56ccc0d261e6f40fffbb8eb04861b7d736a9cf7
Satoshi Shoji <shoji@ga-design.jp>
2018-05-21 11:50:02 +0900

    コーディングデータの追加（対象ページ：素材について、サイズ選びについて、オプションについて）

---
+++ src/size/index.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>サイズ選びについて</title>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="format-detection" content="telephone=no">
+    <link rel="shortcut icon" href="../assets/img/favicon.ico">
+    <link rel="apple-touch-icon" href="../assets/img/apple-touch-icon.png">
+    <link rel="stylesheet" href="../assets/css/common.css">
+    <link rel="stylesheet" href="../assets/css/search.css">
+    <link rel="stylesheet" href="../assets/css/layer.css">
+</head>
+
+<body>
+
+<?php include_once "../inc/header.inc"; ?>
+
+<main class="l-main">
+    <div class="l-inner size">
+        <h1 class="main__title"><img src="../assets/img/size/size_title.png" alt="サイズ選びについて"></h1>
+        <div class="main__content">
+            <ul class="main__breadcrumb">
+                <li><a href="">HOME</a></li>
+                <li><a href="">価格表</a></li>
+                <li>サイズ選びについて</li>
+            </ul>
+
+            <section class="size">
+                <h4 class="ttl01">サイズの選び方</h4>
+                <h5 class="ttl03">デザインから選ぶ</h5>
+                <p class="txt">
+                    デザインは、最初に縦横の比率を決めて探していただきます。同じデザインでも正方形～長方形で印象が変わる為、テンプレート一覧から最もイメージに近い比率をお選びいただき、デザインを編集してください。デザイン決定後、その比率の縦横サイズを決定してください。
+                </p>
+                
+                <h5 class="ttl03">設置場所で選ぶ場合</h5>
+                <p class="txt">
+                    あらかじめ設置場所が決まっている場合は、縦か横だけでもロープや布で実寸を確認することをオススメします。また、屋内か屋外か、どの距離から見るのか、大きさの規定がないかなど、事前にご確認ください。<br>
+                    広い屋外の場合、実際の距離感で設置場所を見ると意外と幕が小さかった、となることが多いです。屋内の場合、幕が大きすぎて設置ができなかった、となることが多いです。
+                </p>
+                
+                <div class="size_hiritsu">
+                    <div class="size_hiritsu1"></div>
+                    <div class="size_hiritsu2"></div>
+                    <div class="size_hiritsu3"></div>
+                </div>
+                
+               <h4 class="ttl01">各サイズの特徴（5 サイズ）</h4>
+                <h5 class="ttl03">縦60cm</h5>
+                <div class="size_right">
+                <div class="size_img60"></div>
+                <div class="size_text">
+                    <ul class="list_dot">
+                    <li>
+                    <p>60×60cm～60×120cmはおひとり様でも手持ちで広げられます。</p></li>
+                    <li>
+                    <p>屋内で掲げて使う場合、大きすぎず、また場所をとりすぎず、オススメです。</p></li>
+                    <li>
+                    <p>高さのない手すりなどに、横長で表示できるのがポイントです。</p></li>
+                    </ul>
+                </div>
+                </div>    
+
+                <h5 class="ttl03">縦90cm</h5>
+                <div class="size_left">
+                <div class="size_img90"></div>
+                <div class="size_text">
+                    <ul class="list_dot">
+                    <li>
+                    <p>屋外スポーツでは、縦90cmの横長幕が多いです。</p></li>
+                    <li>
+                    <p>フェンスなどに取り付けてご使用いただくのがオススメです。</p></li>
+                    <li>
+                    <p>文字が多い場合などは、横180cm以上ですと収まりがいいです。</p></li>
+                    </ul>
+                </div>
+                </div>    
+                
+                <h5 class="ttl03">縦120cm</h5>
+                <div class="size_right">
+                <div class="size_img120"></div>
+                <div class="size_text">
+                    <ul class="list_dot">
+                    <li>
+                    <p>屋内スポーツでは、120×180cmまたは120×240cmが多いです。</p></li>
+                    <li>
+                    <p>屋内競技場で2階等から手すりに下げてお使いいただくのがオススメです。</p></li>
+                    <li>
+                    <p>このサイズまでは、持ち歩くのに便利な幕です。</p></li>
+                    </ul>
+                </div>
+                </div>
+                
+                <h5 class="ttl03">縦150cm</h5>
+                <div class="size_left">
+                <div class="size_img150"></div>
+                <div class="size_text">
+                    <ul class="list_dot">
+                    <li>
+                    <p>屋内、屋外ともインパクトのあるサイズです。</p></li>
+                    <li>
+                    <p>広さのあるフェンスや2階以上の手すり等につけても、遠くからよく目立つ大きさです。</p></li>
+                    </ul>
+                </div>
+                </div>    
+                
+                <h5 class="ttl03">縦180cm<span class="size_p">（※サテン生地は制作できません）</span></h5>
+                <div class="size_right">
+                <div class="size_img180"></div>
+                <div class="size_text">
+                    <ul class="list_dot">
+                    <li>
+                    <p>広い会場でも、迫力満点に目立つサイズです。</p></li>
+                    <li>
+                    <p>掲げる場所のサイズ規定がないか、事前にご確認ください。</p></li>
+                    </ul>
+                </div>
+                </div>
+                
+                
+            <div class="btn_three">
+                <div class="btn_return"><a href="../price/"><p>価格表に戻る</p></a></div>
+                <div class="btn_return"><a href="../material/"><p>素材について</p></a></div>
+                <div class="btn_return"><a href="../option/"><p>オプションについて</p></div>
+            </div>
+                
+            <div class="btn"><a href="#"><img src="../assets/img/top/make_btn.png" alt="ネットでレイアウトを調整しながら自分でデザイン！オリジナル横断幕を作る"></a></div>
+
+            </section>    
+        </div>
+    </div>
+</main>
+<!-- /.l-main -->
+
+<?php include_once "../inc/footer.inc"; ?>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="../assets/js/common.js"></script>
+<script src="../assets/js/search.js"></script>
+</body>
+</html>

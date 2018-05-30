0e79a23d336cebebedcca230bf9762630b632393
Arabbit <tsupo222@yahoo.co.jp>
2018-05-30 16:56:00 +0900

    フロントレイアウト切り分け、会員登録ページnameパラメータ修正

--- resources/views/front/register/index.blade.php
+++ resources/views/front/register/index.blade.php
@@ -62,10 +62,10 @@
                                 <dt>携帯電話番号</dt>
                                 <dd>
                                     <ul class="innerlist_tel">
-                                        <li><input type="text" name="telOne" value="{{old('telOne')}}" id=""/>&emsp;-&emsp;<input
+                                        <li><input type="text" name="telOne" value="{{old('mobileOne')}}" id=""/>&emsp;-&emsp;<input
                                                     type="text" name="telTwo"
-                                                    value="{{old('telTwo')}}" id=""/>&emsp;-&emsp;<input
-                                                    type="text" name="telThree" value="{{old('telThree')}}" id=""/></li>
+                                                    value="{{old('mobileTwo')}}" id=""/>&emsp;-&emsp;<input
+                                                    type="text" name="telThree" value="{{old('mobileThree')}}" id=""/></li>
                                     </ul>
                                 </dd>
                             </dl>
@@ -73,10 +73,10 @@
                                 <dt>自宅電話番号</dt>
                                 <dd>
                                     <ul class="innerlist_tel">
-                                        <li><input type="text" name="mobileOne" value="{{old('mobileOne')}}" id=""/>&emsp;-&emsp;<input
+                                        <li><input type="text" name="mobileOne" value="{{old('telOne')}}" id=""/>&emsp;-&emsp;<input
                                                     type="text" name="mobileTwo"
-                                                    value="{{old('mobileTwo')}}" id=""/>&emsp;-&emsp;<input
-                                                    type="text" name="mobileThree" value="{{old('mobileThree')}}"
+                                                    value="{{old('telTwo')}}" id=""/>&emsp;-&emsp;<input
+                                                    type="text" name="mobileThree" value="{{old('telThree')}}"
                                                     id=""/>
                                         </li>
                                     </ul>
@@ -92,59 +92,12 @@
                             <dl class="innerlist_address add02">
                                 <dt><span>必須</span>都道府県</dt>
                                 <dd><select name="prefecture" id="address1">
-                                        <option value="none" selected="selected">選択して下さい</option>
-                                        <option value="北海道" @if(old('prefecture')=="1") selected @endif>北海道</option>
-                                        <option value="青森県" @if(old('prefecture')=="青森県") selected @endif>青森県</option>
-                                        <option value="岩手県" @if(old('prefecture')=="岩手県") selected @endif>岩手県</option>
-                                        <option value="宮城県" @if(old('prefecture')=="宮城県") selected @endif>宮城県</option>
-                                        <option value="秋田県" @if(old('prefecture')=="秋田県") selected @endif>秋田県</option>
-                                        <option value="山形県" @if(old('prefecture')=="山形県") selected @endif>山形県</option>
-                                        <option value="福島県" @if(old('prefecture')=="福島県") selected @endif>福島県</option>
-                                        <option value="茨城県" @if(old('prefecture')=="茨城県") selected @endif>茨城県</option>
-                                        <option value="栃木県" @if(old('prefecture')=="栃木県") selected @endif>栃木県</option>
-                                        <option value="群馬県" @if(old('prefecture')=="群馬県") selected @endif>群馬県</option>
-                                        <option value="埼玉県" @if(old('prefecture')=="埼玉県") selected @endif>埼玉県</option>
-                                        <option value="千葉県" @if(old('prefecture')=="千葉県") selected @endif>千葉県</option>
-                                        <option value="東京都" @if(old('prefecture')=="東京都") selected @endif>東京都</option>
-                                        <option value="神奈川県" @if(old('prefecture')=="神奈川県") selected @endif>神奈川県
-                                        </option>
-                                        <option value="新潟県" @if(old('prefecture')=="新潟県") selected @endif>新潟県</option>
-                                        <option value="富山県" @if(old('prefecture')=="富山県") selected @endif>富山県</option>
-                                        <option value="石川県" @if(old('prefecture')=="石川県") selected @endif>石川県</option>
-                                        <option value="福井県" @if(old('prefecture')=="福井県") selected @endif>福井県</option>
-                                        <option value="山梨県" @if(old('prefecture')=="山梨県") selected @endif>山梨県</option>
-                                        <option value="長野県" @if(old('prefecture')=="長野県") selected @endif>長野県</option>
-                                        <option value="岐阜県" @if(old('prefecture')=="岐阜県") selected @endif>岐阜県</option>
-                                        <option value="静岡県" @if(old('prefecture')=="静岡県") selected @endif>静岡県</option>
-                                        <option value="愛知県" @if(old('prefecture')=="愛知県") selected @endif>愛知県</option>
-                                        <option value="三重県" @if(old('prefecture')=="三重県") selected @endif>三重県</option>
-                                        <option value="滋賀県" @if(old('prefecture')=="滋賀県") selected @endif>滋賀県</option>
-                                        <option value="京都府" @if(old('prefecture')=="京都府") selected @endif>京都府</option>
-                                        <option value="大阪府" @if(old('prefecture')=="大阪府") selected @endif>大阪府</option>
-                                        <option value="兵庫県" @if(old('prefecture')=="兵庫県") selected @endif>兵庫県</option>
-                                        <option value="奈良県" @if(old('prefecture')=="奈良県") selected @endif>奈良県</option>
-                                        <option value="和歌山県" @if(old('prefecture')=="和歌山県") selected @endif>和歌山県
-                                        </option>
-                                        <option value="鳥取県" @if(old('prefecture')=="鳥取県") selected @endif>鳥取県</option>
-                                        <option value="島根県" @if(old('prefecture')=="島根県") selected @endif>島根県</option>
-                                        <option value="岡山県" @if(old('prefecture')=="岡山県") selected @endif>岡山県</option>
-                                        <option value="広島県" @if(old('prefecture')=="広島県") selected @endif>広島県</option>
-                                        <option value="山口県" @if(old('prefecture')=="山口県") selected @endif>山口県</option>
-                                        <option value="徳島県" @if(old('prefecture')=="徳島県") selected @endif>徳島県</option>
-                                        <option value="香川県" @if(old('prefecture')=="香川県") selected @endif>香川県</option>
-                                        <option value="愛媛県" @if(old('prefecture')=="愛媛県") selected @endif>愛媛県</option>
-                                        <option value="高知県" @if(old('prefecture')=="高知県") selected @endif>高知県</option>
-                                        <option value="福岡県" @if(old('prefecture')=="福岡県") selected @endif>福岡県</option>
-                                        <option value="佐賀県" @if(old('prefecture')=="佐賀県") selected @endif>佐賀県</option>
-                                        <option value="長崎県" @if(old('prefecture')=="長崎県") selected @endif>長崎県</option>
-                                        <option value="熊本県" @if(old('prefecture')=="熊本県") selected @endif>熊本県</option>
-                                        <option value="大分県" @if(old('prefecture')=="大分県") selected @endif>大分県</option>
-                                        <option value="宮崎県" @if(old('prefecture')=="宮崎県") selected @endif>宮崎県</option>
-                                        <option value="鹿児島県" @if(old('prefecture')=="鹿児島県") selected @endif>鹿児島県
-                                        </option>
-                                        <option value="沖縄県" @if(old('prefecture')=="沖縄県") selected @endif>沖縄県</option>
-                                        <option value="日本国外" @if(old('prefecture')=="日本国外") selected @endif>日本国外
-                                        </option>
+                                        @foreach(config('pref') as $name)
+                                            <option value="{{ $name }}"
+                                                    @if(old('prefecture') == $name) selected @endif>{{$name}}
+                                            </option>
+                                        @endforeach
+                                        <option value="日本国外">日本国外</option>
                                     </select></dd>
                             </dl>
                             <dl>

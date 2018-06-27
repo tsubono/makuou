f09128cb71ba69ccc31ed8069a74668f9efec149
Arabbit <tsupo222@yahoo.co.jp>
2018-06-27 16:56:43 +0900

    MAKUOU-28 ヘッダー修正

--- resources/views/front/components/header.blade.php
+++ resources/views/front/components/header.blade.php
@@ -3,17 +3,29 @@
         <div class="left">
             <div class="cf">
                 <h1 class="logo"><a href="/"><img src="{{asset("assets/img/common/logo.png")}}" alt="幕王"></a></h1>
-                <div class="catch"><img src="{{asset("assets/img/common/head_catch.png")}}" alt="オリジナルの横断幕自分でデザイン気軽に発注"></div>
+                <div class="catch"><img src="{{asset("assets/img/common/head_catch.png")}}" alt="オリジナルの横断幕自分でデザイン気軽に発注">
+                </div>
             </div>
         </div>
         <div class="right sp_none">
             <div class="login cf">
-                <p class="name">ようこそゲスト様</p>
-                <p class="btn"><a href="{{url('/login')}}">ログイン</a></p>
+                    <p class="name">
+                        @if(\Illuminate\Support\Facades\Auth::check())
+                        ようこそ{{\Illuminate\Support\Facades\Auth::user()->name}}様
+                        @endif
+                    </p>
+                <p class="btn">
+                    @if(\Illuminate\Support\Facades\Auth::check())
+                        <a href="{{url('/logout')}}">ログアウト</a>
+                    @else
+                        <a href="{{url('/login')}}">ログイン</a>
+                    @endif
+                </p>
             </div>
             <div class="hour">
                 <div><img src="{{asset("assets/img/common/tel.png")}}" alt="0120-805-266"></div>
-                <div class="box_dot"><p>【営業時間】9:00～17:00　<span class="txt_blue">土</span><span class="txt_red">日祝</span>休み</p></div>
+                <div class="box_dot"><p>【営業時間】9:00～17:00　<span class="txt_blue">土</span><span class="txt_red">日祝</span>休み
+                    </p></div>
             </div>
         </div>
         <div class="right pc_none sp-ham">
@@ -44,7 +56,8 @@
     </div>
     <div class="hour">
         <div><a href="tel:0120805266"><img src="{{asset("assets/img/common/tel.png")}}" alt="0120-805-266"></a></div>
-        <div class="box_dot"><p>【営業時間】9:00～17:00　<span class="txt_blue">土</span><span class="txt_red">日祝</span>休み</p></div>
+        <div class="box_dot"><p>【営業時間】9:00～17:00　<span class="txt_blue">土</span><span class="txt_red">日祝</span>休み</p>
+        </div>
     </div>
     <ul class="cf">
         <li><a href="/">HOME</a></li>
